5f7500b2445d6d3ae248c433168536e4036154d4
Hokage09das <dbayizbekov96@gmail.com>
2023-01-25 19:46:32 +0600

    rewritten with mui

--- src/views/content/main/main.view.jsx
+++ src/views/content/main/main.view.jsx
@@ -1,13 +1,12 @@
 import { Header } from "../header";
-import {BlockContainer} from "../../components/containers/block";
+import { BlockContainer } from "../../components/containers/block";
 
 export const Main = (props) => {
-    return (
-        <main>
-            <Header />
-            <BlockContainer>
-                {props.children}
-            </BlockContainer>
-        </main>
-    )
-}
+  return (
+    <main>
+      <Header />
+      {/* <BlockContainer>{props.children}</BlockContainer> */}
+      {props.children}
+    </main>
+  );
+};
