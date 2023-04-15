a061c63fea6bd83bced91d5d274cb8f950ca391c
Hokage09das <dbayizbekov96@gmail.com>
2023-04-15 18:09:12 +0600

    media init subject_create_page

--- src/views/components/elements/multi-select/multi-select.jsx
+++ src/views/components/elements/multi-select/multi-select.jsx
@@ -4,36 +4,36 @@ import { Check } from '@mui/icons-material';
 import { Autocomplete, MenuItem, TextField } from '@mui/material';
 
 export const MultiSelect = ({ options, value, setValue, label }) => {
-  return (
-    <Autocomplete
-      sx={{ m: 1, width: 500 }}
-      multiple
-      options={options}
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      getOptionLabel={(option) => ` ${option.firstName} ${option.lastName}`}
-      disableCloseOnSelect
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant='outlined'
-          label={label}
-        />
-      )}
-      renderOption={(props, option, { selected }) => (
-        <MenuItem
-          {...props}
-          key={option._id}
-          value={`${option.firstName} ${option.lastName}`}
-          sx={{ justifyContent: 'space-between' }}
-        >
-          {`${option.firstName} ${option.lastName}`}
+	return (
+		<Autocomplete
+			sx={{ m: 1, width: '100%' }}
+			multiple
+			options={options}
+			value={value}
+			onChange={(event, newValue) => {
+				setValue(newValue);
+			}}
+			getOptionLabel={(option) => ` ${option.firstName} ${option.lastName}`}
+			disableCloseOnSelect
+			renderInput={(params) => (
+				<TextField
+					{...params}
+					variant='outlined'
+					label={label}
+				/>
+			)}
+			renderOption={(props, option, { selected }) => (
+				<MenuItem
+					{...props}
+					key={option._id}
+					value={`${option.firstName} ${option.lastName}`}
+					sx={{ justifyContent: 'space-between' }}
+				>
+					{`${option.firstName} ${option.lastName}`}
 
-          {selected ? <Check color='info' /> : null}
-        </MenuItem>
-      )}
-    />
-  );
+					{selected ? <Check color='info' /> : null}
+				</MenuItem>
+			)}
+		/>
+	);
 };
---
+++ src/views/pages/admin/sub-pages/subject-page/subject-sub-page/subject-list/constants.js
@@ -0,0 +1,29 @@
+export const semesters = [
+	{
+		label: 'Fall Semester',
+		value: 1,
+	},
+	{
+		label: 'Spring Semester',
+		value: 2,
+	},
+];
+
+export const courses = [
+	{
+		id: '1',
+		title: '1 course',
+	},
+	{
+		id: '2',
+		title: '2 course',
+	},
+	{
+		id: '3',
+		title: '3 course',
+	},
+	{
+		id: '4',
+		title: '4 course',
+	},
+];
