8e8cebb8cb0996a57d071b405a385b7c3034ce65
begalievn <begaliev.info@gmail.com>
2023-05-08 22:56:24 +0600

    fix: Fixed ratdio group value

--- src/views/pages/admin/teacher-page/teacher-update/teacher-update.view.jsx
+++ src/views/pages/admin/teacher-page/teacher-update/teacher-update.view.jsx
@@ -24,6 +24,7 @@ import {genderOptions, inputFields, workDays, workingHoursOptions} from '../cons
 // styles
 import 'react-toastify/dist/ReactToastify.css';
 import classes from "./style.module.scss";
+import {ContainerWithLoader} from '../../../../components/containers/container-with-loader';
 
 export const TeacherUpdate = () => {
   const [teacherData, setTeacherData] = useState({
@@ -32,11 +33,11 @@ export const TeacherUpdate = () => {
     birthday: null,
     email: '',
     address: '',
-    gender: '',
+    gender: genderOptions[0].value,
     phone: '',
     experience: '',
     workingDays: [],
-    workingGraphic: '',
+    workingGraphic: workingHoursOptions[0].value,
   });
   const [selectedDays, setSelectedDays] = useState([]);
   const [openConfirmModal, setOpenConfirmModal] = useState(false);
@@ -49,11 +50,8 @@ export const TeacherUpdate = () => {
     skip: false,
   });
   
-  const [updateTeacher, { isSuccess: isTeacherUpdateSuccess, isError: isTeacherUpdateError }] = useUpdateTeacherMutation();
+  const [updateTeacher] = useUpdateTeacherMutation();
   const [deleteTeacher, { isSuccess: isTeacherDeleteSuccess, isError: isTeacherDeleteError }] = useDeleteTeacherByIdMutation();
-  
-  console.log('dayjs', dayjs(new Date()).format('YYYY-MM-DD'));
-  console.log(typeof dayjs(new Date()).format('YYYY-MM-DD'));
   
   const handleBirthdayChange = (event) => {
     const value = dayjs(event);
@@ -70,7 +68,6 @@ export const TeacherUpdate = () => {
   }
   
   const handleInputsChange = (e) => {
-    console.log(e);
     setTeacherData({...teacherData, [e.target.name]: e.target.value});
   }
   
@@ -92,7 +89,6 @@ export const TeacherUpdate = () => {
     e.preventDefault()
     
     const birthdayValue = dayjs(teacherData?.birthday || null).toDate();
-    console.log('&&&&',dayjs(teacherData.birthday).toDate());
     
     const body = JSON.parse(JSON.stringify({
       ...teacherData,
@@ -102,7 +98,6 @@ export const TeacherUpdate = () => {
     
     const result = await updateTeacher({ id: params?.id, body});
     handleToast(result);
-    console.log('result', result);
   }
   
   const handeOpenConfirmModal = () => {
@@ -128,8 +123,6 @@ export const TeacherUpdate = () => {
   }
   
   useEffect(() => {
-    console.log('useEffect', teacherById);
-    console.log('teacherById', teacherById);
     const birthdayValue = dayjs(teacherById?.birthday || null);
     setTeacherData({
       ...teacherById,
@@ -137,63 +130,63 @@ export const TeacherUpdate = () => {
       workingDays: [...teacherById?.workingDays || []],
     })
     setSelectedDays([...teacherById?.workingDays || []]);
-    
-    console.log('***', teacherById?.birthday);
-    console.log('*** dayjs', birthdayValue);
-  }, [teacherById])
-  console.log('teach', teacherData);
-  console.log('workingGraphic state', teacherData?.workingGraphic);
+  }, [teacherById]);
+  
   return (
     <ContentContainer style={{marginBottom: '50px'}}>
       <HeaderV1>Teacher update</HeaderV1>
-      <form onSubmit={handleSubmit}>
-        <div className={classes.form_container}>
-          <ContainerWithShadow>
-            <div className={classes.first_container}>
-              {
-                inputFields.map((field) => (
-                  <label key={field.id} className={classes.form_label}>
-                    {field.label}
-                    <Input name={field.name} value={teacherData[field.name]} onChange={handleInputsChange} placeholder={field.label}/>
-                  </label>
-                ))
-              }
-              <label className={classes.form_label}>
-                Birth date
-                <DatePickerV2 value={teacherData.birthday} onChange={handleBirthdayChange} />
-              </label>
-            </div>
-          </ContainerWithShadow>
-          <ContainerWithShadow style={{background: '#E5EFFE'}}>
-            <div className={classes.first_container}>
-              <label className={classes.form_label}>
-                Gender
-                <RadioGroupV2 onChange={handleInputsChange} value={teacherData?.gender} name={'gender'} options={genderOptions} />
-              </label>
-              <label className={classes.form_label}>
-                Working graphic
-                <RadioGroupV2 onChange={handleInputsChange} value={teacherData?.workingGraphic} name={'workingGraphic'} options={workingHoursOptions} />
-              </label>
-              <label className={classes.form_label}>
-                Working days
-                <div className={classes.check_box_container}>
+      {
+        isLoading ? <ContainerWithLoader style={{ height: '500px' }}/> :
+          <form onSubmit={handleSubmit}>
+            <div className={classes.form_container}>
+              <ContainerWithShadow>
+                <div className={classes.first_container}>
                   {
-                    workDays.map((day) => (
-                      <Fragment key={day.day}>
-                        <CheckBoxV2 label={day.day} checked={selectedDays.includes(day.value)} value={day.value} handleChange={handleDaySelect} />
-                      </Fragment>
+                    inputFields.map((field) => (
+                      <label key={field.id} className={classes.form_label}>
+                        {field.label}
+                        <Input name={field.name} value={teacherData[field.name]} onChange={handleInputsChange} placeholder={field.label}/>
+                      </label>
                     ))
                   }
+                  <label className={classes.form_label}>
+                    Birth date
+                    <DatePickerV2 value={teacherData.birthday} onChange={handleBirthdayChange} />
+                  </label>
                 </div>
-              </label>
-              <div className={classes.buttons}>
-                <ButtonV2 type="submit">Update</ButtonV2>
-                <ButtonV2 onClick={handeOpenConfirmModal} style={{background: 'white', color: '#cc5092'}}>Delete</ButtonV2>
-              </div>
+              </ContainerWithShadow>
+              <ContainerWithShadow style={{background: '#E5EFFE'}}>
+                <div className={classes.first_container}>
+                  <label className={classes.form_label}>
+                    Gender
+                    <RadioGroupV2 onChange={handleInputsChange} value={teacherData.gender} name={'gender'} options={genderOptions} />
+                  </label>
+                  <label className={classes.form_label}>
+                    Working graphic
+                    <RadioGroupV2 onChange={handleInputsChange} value={teacherData.workingGraphic} name={'workingGraphic'} options={workingHoursOptions} />
+                  </label>
+                  <label className={classes.form_label}>
+                    Working days
+                    <div className={classes.check_box_container}>
+                      {
+                        workDays.map((day) => (
+                          <Fragment key={day.day}>
+                            <CheckBoxV2 label={day.day} checked={selectedDays.includes(day.value)} value={day.value} handleChange={handleDaySelect} />
+                          </Fragment>
+                        ))
+                      }
+                    </div>
+                  </label>
+                  <div className={classes.buttons}>
+                    <ButtonV2 type="submit">Update</ButtonV2>
+                    <ButtonV2 onClick={handeOpenConfirmModal} style={{background: 'white', color: '#cc5092'}}>Delete</ButtonV2>
+                  </div>
+                </div>
+              </ContainerWithShadow>
             </div>
-          </ContainerWithShadow>
-        </div>
-      </form>
+          </form>
+      }
+      
       <AlertDialog
         open={openConfirmModal}
         handleAction={handleDeleteTeacher}
