e6033073394fac4d09960628330fdc0877c85f15
begalievn <begaliev.info@gmail.com>
2023-05-19 00:30:35 +0600

    feat: Added sending data to generator

--- src/views/pages/admin/schedule-page/schedule-send/schedule-send-page.view.jsx
+++ src/views/pages/admin/schedule-page/schedule-send/schedule-send-page.view.jsx
@@ -3,15 +3,26 @@ import { useNavigate } from "react-router-dom";
 import { HeaderV1 } from "../../../../components/elements/header-v1";
 import { ButtonV2 } from "../../../../components/elements/button-v2";
 import {Container} from "@mui/material";
+import {useSelector} from 'react-redux';
 
 // styles
 import classes from './style.module.scss';
-import {useSelector} from 'react-redux';
+import {useGetClassroomsQuery} from '../../../../../store/api/classroom-api';
 
 export const ScheduleSendPage = () => {
+  const selectedSubjects = useSelector(state => state.schedule.selectedSubjects);
+  
+  const { data: classrooms, isLoading: classroomsLoading } = useGetClassroomsQuery();
+  
   const navigate = useNavigate();
   
-  const selectedSubjects = useSelector(state => state.schedule.selectedSubjects);
+  const handleSendToGenerator = () => {
+    const data = {
+      subjects: selectedSubjects,
+      classrooms: classrooms,
+    }
+    console.log(data);
+  }
   
   const handleContinueManually = () => {
     navigate('/admin/schedule/table')
@@ -25,11 +36,21 @@ export const ScheduleSendPage = () => {
           <div className={classes.amount}>Amount: <span>{selectedSubjects.length}</span></div>
         </div>
         <div className={classes.blocks}>
-          <ButtonV2 style={{width: '100%', marginBottom: '30px'}}>Send to the schedule generator</ButtonV2>
+          <ButtonV2
+            onClick={handleSendToGenerator}
+            style={{width: '100%', marginBottom: '30px'}}
+          >
+            Send to the schedule generator
+          </ButtonV2>
           <p className={classes.status}>Status: <span className={classes.status_span}>IN PROGRESS</span></p>
         </div>
         <div className={classes.blocks}>
-          <ButtonV2 style={{width: '100%'}} onClick={ handleContinueManually }>Continue manually</ButtonV2>
+          <ButtonV2
+            style={{width: '100%'}}
+            onClick={ handleContinueManually }
+          >
+            Continue manually
+          </ButtonV2>
         </div>
       </div>
     </Container>
